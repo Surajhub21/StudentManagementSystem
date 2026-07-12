ed4505968c27fb8c100e8cdf3615127b9c977a16
Ujjwal Kumar <ujjwal.live0@email.com>
2026-07-12 23:36:57 +0530

    Updated by Ujjwal KUmar

--- src/main/java/com/developersuraj/exceptions/StudentNotFoundException.java
+++ src/main/java/com/developersuraj/exceptions/StudentNotFoundException.java
@@ -2,7 +2,7 @@ package com.developersuraj.exceptions;
 
 
 public class StudentNotFoundException extends RuntimeException {
-
+//student Management system
     public StudentNotFoundException(int id) {
         super("Student with ID " + id + " not found.");
     }
